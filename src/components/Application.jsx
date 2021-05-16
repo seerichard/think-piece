025c5333a4f009af0c2f4a109ecfb6dd9eee7800
Richard See <richard.see@argos.co.uk>
2021-05-16 12:45:34 +0100

    Authentication UI

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 import { firestore } from "../firebase";
+import Authentication from "./Authentication";
 import Posts from "./Posts";
 import { collectIdsAndDocs } from "../utilities";
 
 const Application = () => {
   const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const unsubscribe = firestore.collection("posts").onSnapshot((snapshot) => {
@@ -19,6 +21,7 @@ const Application = () => {
   return (
     <main className="Application">
       <h1>Think Piece</h1>
+      <Authentication user={user} />
       <Posts posts={posts} />
     </main>
   );
