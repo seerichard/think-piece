9e61b899322d3849a3a52ca345ad622870407f0b
Richard See <richard.see@argos.co.uk>
2021-05-16 13:07:43 +0100

    Sign Out

--- src/firebase.js
+++ src/firebase.js
@@ -22,6 +22,8 @@ export const auth = firebase.auth();
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export const signOut = () => auth.signOut();
+
 // Not exactly best practice, but great for debugging!
 window.firebase = firebase;
 
